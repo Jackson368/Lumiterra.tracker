498f71e48f94c976597c7d453427970e8f5f6900
Jackson368 <arayinka15@gmail.com>
2025-09-09 04:57:58 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+async function fetchData() {
+  const walletAddress = document.getElementById("walletAddress").value;
+  document.getElementById("results").innerText = `Fetching data for ${walletAddress}...`;
+  // API logic will go here
+}
+
+
